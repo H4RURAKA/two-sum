f7168ae06c2e18611f73a161c4692805a32f4c1b
H4RURAKA <qwdfgqw@gmail.com>
2023-10-04 18:56:40 +0900

    add README, working twoSum Func.

---
+++ README.md
@@ -0,0 +1,3 @@
+Code that return the indices of two integers from the given array "nums" that add up to the specified integer "target”.
+
+Caution : Assume that only one possible solution exists, and each array element can only be used once. (cannot be used twice or more)
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from typing import List
+
 def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+    visit_nums = {}
+
+    for i, num in enumerate(nums) :
+        gap = target - num
+        if gap in visit_nums :
+            return [visit_nums[gap], i]
+        visit_nums[num] = i
     
     return
